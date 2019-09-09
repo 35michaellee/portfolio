cde5e4686b72545caa2b47d519541ecb60dc4424
michael esparaz <mikeesparza@Mikes-MacBook-Pro.local>
2019-09-09 09:58:31 -0700

    simplified portfolio for advice

--- portfolio.js
+++ portfolio.js
@@ -1,1 +1,15 @@
-$(".forProjects")
+$("#githublink").on("click",function(event){
+    window.location.href = "https://github.com/35michaellee";
+});
+$("#linkedinlink").on("click",function(event){
+    console.log("hello");
+    window.location.href = "https://www.linkedin.com/in/michael-esparza-075816b2/";
+});
+$("#otherwork").on("click",function(event){
+    <a href="#get_in_touch">Go to the anchor</a>
+})
+
+$(document).ready(function() {
+   $('#introdiv').localScroll({duration:800});
+});
+
